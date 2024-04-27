c5e81b5b06627f1a09e5085e3707b5617add54f6
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-04-27 15:32:45 +0600

    create card

--- src/components/FoodCard.jsx
+++ src/components/FoodCard.jsx
@@ -1,4 +1,4 @@
-
+import { FaHeart } from "react-icons/fa";
 
 // eslint-disable-next-line react/prop-types
 const FoodCard = ({data}) => {
@@ -6,9 +6,9 @@ const FoodCard = ({data}) => {
   console.log(data)
 
   return (
-    <div className="m-2">
+    <div className="m-2 shadow-lg rounded-lg">
       {/* card section start */}
-      <div className="card w-[100%] glass shadow-xl">
+      <div className="card w-[100%] glass ">
         <figure>
           <img
             src={data.image}
@@ -16,10 +16,27 @@ const FoodCard = ({data}) => {
           />
         </figure>
         <div className="card-body bg-zinc-100">
-          <h2 className="xl:text-base font-bold">{data.name}</h2>
-          <p>{data.recipe}</p>
-          <div className="card-actions justify-end">
-            <button className="xl:px-3 xl:py-2 rounded-lg bg-orange-600 text-white xl:text-xs font-bold">Add to Card</button>
+          {/* reviews & price start */}
+          <div className="flex justify-between items-center">
+            {/* reviews start */}
+            <p className="flex items-center gap-1"><span className="text-sm">Rating</span> <div className="rating rating-sm">
+              <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-600" required/>
+              <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-600" checked required/>
+              <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-600" required/>
+              <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-600" required/>
+              <input type="radio" name="rating-6" className="mask mask-star-2 bg-orange-600" required/>
+            </div></p>
+            {/* reviews end */}
+            {/* price start */}
+            <p className="xl:text-xl font-bold">${data.price}</p>
+            {/* price end */}
+          </div>
+          {/* reviews & price end */}
+          <h2 className="xl:text-base font-bold italic">{data.name}</h2>
+          <p className="xl:text-sm">{data.recipe}</p>
+          <div className="flex justify-between items-center">
+            <button className="bg-orange-600 p-2 rounded-full text-white"><FaHeart></FaHeart></button>
+            <button className="xl:px-3 xl:py-2 rounded-md bg-orange-600 text-white xl:text-xs font-bold">Add to Card</button>
           </div>
         </div>
       </div>
