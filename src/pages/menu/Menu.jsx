75695175e11691398b33e9e2e411def79d87a542
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-04-29 21:12:41 +0600

    adding menu data in footCard

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,11 +1,12 @@
 import { useEffect, useState } from "react";
 import MenuBanner from "./MenuBanner";
+import FoodCard from './../../components/FoodCard';
 
 
 const Menu = () => {
 
   //menu data
-  const [menu, setMen] = useState([]);
+  const [menu, setMenu] = useState([]);
 
   //loading menu data
   useEffect(() => {
@@ -13,7 +14,7 @@ const Menu = () => {
       try{
         const response = await fetch('menu.json')
         const data = await response.json();
-        console.log(data)
+        setMenu(data)
       }catch(error){
         console.log(error.message)
       }
@@ -22,11 +23,25 @@ const Menu = () => {
   },[])
 
   return (
-    <div>
+    <div className="display">
       {/* menuBanner section start */}
       <MenuBanner></MenuBanner>
       {/* menuBanner section end */}
       
+      {/* menu data section start */}
+      {/* button start */}
+      <div className="flex gap-2">
+        
+      </div>
+      {/* button end */}
+      {/* menu content start */}
+      <div className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4 gap-3">
+        {
+          menu.map((data, index) => <FoodCard key={index} data={data}></FoodCard>)
+        }
+        {/* menu content end */}
+      </div>
+      {/* menu data section end */}
     </div>
   );
 };
