e0fd7bc1fab68a1906d1bfefc309c533706c7219
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-04-29 21:06:46 +0600

    loading menu data in async function

--- src/pages/menu/Menu.jsx
+++ src/pages/menu/Menu.jsx
@@ -1,7 +1,26 @@
+import { useEffect, useState } from "react";
 import MenuBanner from "./MenuBanner";
 
 
 const Menu = () => {
+
+  //menu data
+  const [menu, setMen] = useState([]);
+
+  //loading menu data
+  useEffect(() => {
+    const loadingFun = async () => {
+      try{
+        const response = await fetch('menu.json')
+        const data = await response.json();
+        console.log(data)
+      }catch(error){
+        console.log(error.message)
+      }
+    }
+    loadingFun();
+  },[])
+
   return (
     <div>
       {/* menuBanner section start */}
