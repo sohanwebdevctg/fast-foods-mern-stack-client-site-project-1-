fd0ba1c0f8e6ffb6f5c7e6a79842a9329aa85b72
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-02 19:42:43 +0600

    create firebase file and install new firebase

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,14 +1,20 @@
 import { useState } from "react";
 import { createContext } from "react";
+import { getAuth } from "firebase/auth";
+import app from './../firebase/firebase.config';
 
 
 //auth context
 export const AuthContext = createContext(null)
+const auth = getAuth(app);
 
+// eslint-disable-next-line react/prop-types
 const AuthProvider = ({children}) => {
 
   //user
   const [user, setUser] = useState(null)
+  // loading 
+  const [loading, setLoading] = useState(true)
 
 
   //user information
