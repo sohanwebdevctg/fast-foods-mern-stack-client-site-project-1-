9ca73a95b602bc96edb1b3701aa5b790727a7ab4
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-03 16:34:06 +0600

    create private route

--- src/layout/MainLayout.jsx
+++ src/layout/MainLayout.jsx
@@ -1,24 +1,34 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import { useContext } from "react";
+import { AuthContext } from "../context/AuthProvider";
+import Loading from "../components/Loading";
 
 
 const MainLayout = () => {
+
+  //authProvider
+  const {user, loading} = useContext(AuthContext)
   
   return (
 
     <div className="bg-zinc-100">
-      {/* navbar section start */}
-      <Navbar></Navbar>
-      {/* navbar section end */}
-      {/* mainLayout section start */}
-      <div className="md:min-h-[700px] pt-16 ">
-        <Outlet></Outlet>
-      </div>
-      {/* mainLayout section end */}
-      {/* footer section start */}
-      <Footer></Footer>
-      {/* footer section end */}
+      {
+        user && loading ? <Loading></Loading> : <div>
+          {/* navbar section start */}
+        <Navbar></Navbar>
+        {/* navbar section end */}
+        {/* mainLayout section start */}
+        <div className="md:min-h-[700px] pt-16 ">
+          <Outlet></Outlet>
+        </div>
+        {/* mainLayout section end */}
+        {/* footer section start */}
+        <Footer></Footer>
+        {/* footer section end */}
+        </div>
+      }
     </div>
 
   );
---
+++ src/privateRoute/PrivateRoute.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useContext } from 'react';
+import { AuthContext } from './../context/AuthProvider';
+import Loading from '../components/Loading';
+import { Navigate } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+// eslint-disable-next-line react/prop-types
+const PrivateRoute = ({children}) => {
+
+  //authProvider
+  const {user, loading} = useContext(AuthContext)
+
+  //locating
+  const location = useLocation();
+
+  //loading
+  if(loading){
+    return <Loading></Loading>
+  }
+
+  //user
+  if(user){
+    return children
+  }
+
+  return <Navigate to="/login" state={{from: location}} replace></Navigate>
+};
+
+export default PrivateRoute;
