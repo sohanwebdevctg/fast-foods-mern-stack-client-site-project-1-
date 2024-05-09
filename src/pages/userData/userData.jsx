1b5ffd09f3821239c36f667041ac038afdcafbdd
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-09 23:31:20 +0600

    adding delete button in data table

--- src/pages/userData/userData.jsx
+++ src/pages/userData/userData.jsx
@@ -1,5 +1,6 @@
 import useCarts from "../../hooks/useCarts";
 import UserDataBanner from "./UserDataBanner";
+import { MdDelete } from "react-icons/md";
 
 const UserData = () => {
   //userCarts data
@@ -17,21 +18,22 @@ const UserData = () => {
         <table className="table">
           {/* head */}
           <thead className="bg-orange-600 text-gray-200">
-            <tr>
+            <tr className="text-center">
               <th>#</th>
               <th>Image</th>
               <th>Name</th>
               <th>Quantity</th>
+              <th>Price</th>
               <th>Action</th>
             </tr>
           </thead>
           <tbody>
             {/* row */}
             {
-              carts.map((data, index) => <tr key={index}>
+              carts.map((data, index) => <tr key={index} className="text-center hover:bg-gray-200">
               <th>{++index}</th>
               <td>
-                <div className="flex items-center gap-3">
+
                   <div className="avatar">
                     <div className="mask mask-squircle w-12 h-12">
                       <img
@@ -40,30 +42,21 @@ const UserData = () => {
                       />
                     </div>
                   </div>
-                </div>
+
               </td>
               <td>
                 <span>Name: {data.name}</span><br></br>
                 <span>Category: {data.category}</span>
               </td>
               <td>{data.quantity}</td>
+              <td>${data.price}</td>
               <th>
-                <button className="btn btn-xs bg-red-600 text-white hover:bg-red-600">Delete</button>
+                <MdDelete className="text-3xl bg-red-600 text-white text-center mx-auto p-1 rounded-md"></MdDelete>
               </th>
             </tr>)
             }
             {/* row */}
           </tbody>
-          {/* foot */}
-          <tfoot>
-          <tr>
-              <th>#</th>
-              <th>Image</th>
-              <th>Name</th>
-              <th>Quantity</th>
-              <th>Action</th>
-            </tr>
-          </tfoot>
         </table>
       </div>
       {/* table section end */}
