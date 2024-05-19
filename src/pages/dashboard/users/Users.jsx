5366086abed2c67e410e024b12ecbd07a3792f4b
sohanwebdevctg <amirhossainsohan0@gmail.com>
2024-05-19 15:21:00 +0600

    creating admin in data base

--- src/pages/dashboard/users/Users.jsx
+++ src/pages/dashboard/users/Users.jsx
@@ -1,9 +1,83 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import { MdDelete } from "react-icons/md";
+import { FaUsers } from "react-icons/fa";
+import Swal from 'sweetalert2';
 
 const Users = () => {
+
+  //user fetch in database
+  const { data : users = [], refetch } = useQuery({
+    queryKey: ['todos'],
+    queryFn: async () => {
+      const res = await fetch('http://localhost:5000/users')
+      return res.json()
+    },
+  })
+
+  //create admin
+  const makeAdmin = (data) => {
+    //fetching data
+    fetch(`http://localhost:5000/users/admin/${data._id}`,{
+      method: 'PATCH'
+    })
+    .then((res) => res.json())
+    .then((data) => {
+      if(data.modifiedCount > 0){
+        refetch()
+        Swal.fire({
+          position: "center",
+          icon: "success",
+          title: "You are admin now",
+          showConfirmButton: false,
+          timer: 1500
+        });
+      }
+    })
+  }
+
+
   return (
-    <div>
-      <h1>user section start</h1>
+    <div className='w-2/3'>
+      {/* user content section start */}
+      {/* table section start */}
+      <div className="my-12 w-full">
+        <table className="table shadow-xl">
+          {/* head */}
+          <thead className="bg-orange-600 text-gray-200">
+            <tr className="text-center">
+              <th>#</th>
+              <th>Name</th>
+              <th>Email</th>
+              <th>Role</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            {/* row */}
+            {
+              users.map((data, index) => <tr key={index} className="text-center hover:bg-gray-200">
+                <td>{++index}</td>
+                {/* name section start */}
+                <td>{data.name}</td>
+                {/* name section end */}
+                {/* email section start */}
+                <td>{data.email}</td>
+                {/* email section end */}
+                {/* role section start */}
+                <td>{data.role === 'admin' ? <p className='text-base text-green-600'>Admin</p> : <FaUsers onClick={() => makeAdmin(data)} className='text-2xl mx-auto'></FaUsers>}</td>
+                {/* role section end */}
+                {/* delete section start */}
+                <td><MdDelete className='bg-red-600 p-[2px] text-2xl text-white rounded-sm mx-auto'></MdDelete></td>
+                {/* delete section end */}
+              </tr>)
+            }
+            {/* row */}
+          </tbody>
+        </table>
+      </div>
+      {/* table section end */}
+      {/* user content section end */}
     </div>
   );
 };
